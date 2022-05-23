584517260bacf670d37540d62aa1d37c2b466192
mgraho <matija.graho@gmail.com>
2022-05-23 11:09:03 +0200

    test nove skripte

--- src/novo.py
+++ src/novo.py
@@ -20,11 +20,10 @@ class Rssi():
         
     def __init__(self):
         self.A = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
-        self.pub = rospy.Publisher("device",Num,queue_size=1)
-        self.rate = rospy.Rate(10) #10 hz 
+        #pub = rospy.Publisher("device",Num,queue_size=1)
+        self.rate = rospy.Rate(1) #1 hz 
         self.device = Num()
         rospy.Subscriber("device",Num, self.callback)
-	
 
     def run(self):
         while not rospy.is_shutdown():
@@ -32,41 +31,42 @@ class Rssi():
             test.name="rpi2"
             test.rssi=-70
             test.sender="rpi0"
-            self.pub.publish(test)
-            self.d = MyDiscoverer()
+            self.d = Rssi.MyDiscoverer()
             self.d.find_devices(lookup_names = True)
-            readfiles = [ self.d, ]
-            while True:
+            found_device = self.d.process_event()
+            
+            #readfiles = [ self.d, ]
+          #  while True:
                 #vise uredaja
-                rfds = select.select( readfiles, [], [] )[0]
-                if self.d in rfds:
-                    self.d.process_event()
-                if self.d.done: break
+            #    rfds = select.select( readfiles, [], [] )[0]
+                #if self.d in rfds:
+                    #self.d.process_event()
+                    #test = self.d.process_event()
+                #if self.d.done: break
             self.rate.sleep()
-    
-class MyDiscoverer(bluetooth.DeviceDiscoverer):
+    class MyDiscoverer(bluetooth.DeviceDiscoverer):
 
-    def pre_inquiry(self):
-        self.done = False
+        def pre_inquiry(self):
+            self.done = False
 
-    def device_discovered(self, address, device_class, rssi, name):
-        print("%s - %s" % (address, name))                              
-        print("  RSSI: " + str(rssi))
-        udaljenost=10**((-62-rssi)/(10*2))
-        print("  udaljenost: " + str(udaljenost))
-        device=Num()
-        device.rssi=rssi
-        device.address=address
-        device.sender="rpix"
-        device.name=name.decode('UTF-8')
-       # pub.publish(device)
-        
-    def inquiry_complete(self):
-        self.done = True
+        def device_discovered(self, address, device_class, rssi, name):
+            print("%s - %s" % (address, name))                              
+            print("  RSSI: " + str(rssi))
+            udaljenost=10**((-62-rssi)/(10*2))
+            print("  udaljenost: " + str(udaljenost))
+            self.device=Num()
+            self.device.rssi=rssi
+            self.device.address=address
+            self.device.sender="rpix"
+            self.device.name=name.decode('UTF-8')
+            pub.publish(self.device)
+            return self.device
+        def inquiry_complete(self):
+            self.done = True
         
 if __name__ == '__main__':
     rospy.init_node('pyclass')
-
+    pub = rospy.Publisher("device",Num,queue_size=1)
     try:
         ne = Rssi()
         ne.run()
