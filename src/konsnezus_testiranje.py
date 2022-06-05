c801471e2c1eb41a0fda03f25c8b6af1e2cf8c6e
mgraho <matija.graho@gmail.com>
2022-06-05 13:07:19 +0200

    konsenzus2

--- src/konsnezus_testiranje.py
+++ src/konsnezus_testiranje.py
@@ -11,30 +11,50 @@ from scipy.optimize import minimize
 
 
 
-def objective(x,Y):
-        n = size(Y, 1);
-        X = [[0, 0] [0, x(1)]];
-        X = [[X][ numpy.reshape(x,(2,end))]];
+def objective(x):
+        Y=[[0,3,4],[3,0,5],[4,5,0]]
+        n = len(Y)
+        #X = [[0, 0] ,[0, x[0]],[x[1], x[2]]]
+        X=[[0, 0] ,[0, x[0]]]
+        x=numpy.delete(x,1)
+       # X = [[X],[ numpy.reshape(x,(1,2))]]
+        X.append(numpy.reshape(x,(1,2)))
+       # print(X)
 
-
-        mse = 0;
-        c = 0;
+        mse = 0
+        c = 0
         for i in range(n):
             for j in range(n):
                 if (i != j):
-                    d = norm(X[i,:] - X[j,:]);
-                    if Y(i,j) > 0:
-                        mse = mse + (d - Y(i, j))^ 2;
-                        c = c + 1;
+                    d = numpy.linalg.norm(numpy.subtract(X[i], X[j]))
+                    #print(numpy.subtract(X[i], X[j]))
+                    if Y[i][j] > 0:
+                        
+                        mse = mse + (d - float(Y[i][j]))** 2
+                        c = c + 1
 
-        mse = mse / c;
+        mse = mse / c
 
         return mse
        
        
        
-if __name__ == '__main__':     
-    matrica_za_testiranje=[[0,3,5][3,0,5][5,5,0]]
+if __name__ == '__main__':  
+    n=3   
+    matrica_za_testiranje=[[0,3,5],[3,0,5],[5,5,0]]
     x0=numpy.zeros((1,n*2-3))
-    sol=minimize(objective(x,matrica_za_testiranje),x0)
+    #print (x0)
+    bnds = ((0, None), (0, None))
+   # sol=minimize(objective,x0,method='SLSQP',bounds=bnds)
+    sol=minimize(objective,x0)
     print(sol.fun)
+    print(sol.x)
+    est=sol.x
+    X_est=[[0,0],[0,est[0]],[est[1],est[2]]]
+    print(X_est)
+    
+    
+    x=[1,2,3]
+    X = [[0, 0] ,[0, x[1]]]
+    #print(X[0])
+    
